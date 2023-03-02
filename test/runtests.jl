489769b81192302caf69d7f1aa57c08bb3c0049b
carlodev <carlobrunellibs@gmail.com>
2023-03-02 10:50:58 +0100

    read from web test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using AirfoilGmsh
 using Test
 
+
 @testset "AirfoilGmsh.jl" begin
-    # Write your tests here.
+    # Test reading from web
+    url = "https://m-selig.ae.illinois.edu/ads/coord/c141a.dat"
+    @test from_url_to_csv(url) == "c141a.csv"
+
+
+    
 end
